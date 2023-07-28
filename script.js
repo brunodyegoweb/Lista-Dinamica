c99eb92bf6fe67f7cea38eacf93e25ec4a4feaef
Bruno Dyego Silveira da Silva <bruno.ufra@yahoo.com.br>
2023-07-28 10:10:34 -0300

    Create Dinamiac list

--- script.js
+++ script.js
@@ -7,12 +7,28 @@ let tarefas = []
 let textelement = document.createTextNode('Cadastrar')
 buttonElement.appendChild(textelement)
 
+function renderTarefa() {
+    
+    listElement.innerHTML = ""
+
+    tarefas.map((name) => {
+        let liElement = document.createElement("li")
+        let textLi = document.createTextNode(name)
+
+        listElement.appendChild(liElement)
+        liElement.appendChild(textLi)
+    })
+}
+
 function cadastrarTarefa() {
     if(inputElement.value === "") {
         alert('[ERRO]Preencha o campo corretamente!')
     }else {
-        alert(inputElement.value)
+       let novaTarefa = inputElement.value
+       tarefas.push(novaTarefa)
         inputElement.value = ''
+
+        renderTarefa()
     }
 }
 
